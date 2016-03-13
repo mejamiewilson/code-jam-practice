5971dd1c32934d6d10290f841379d64bcae038cb
Jamie Wilson <jamie@hoist.io>
2016-03-13 16:07:28 +1300

    cookie clicker

---
+++ Cookie Clicker/run.js
@@ -0,0 +1,52 @@
+/* Large File */
+var fs = require('fs');
+
+var cookie = function(set) {
+	
+	var seconds = 0;
+	var rate = 2;
+
+	while (rate / set.F < (set.X / set.C) -1) {
+		
+		seconds = seconds + ( set.C / rate );
+		rate = rate + set.F;
+		
+	}
+	seconds = seconds + set.X / rate;
+	
+	return seconds;
+
+};
+
+/* File Function */
+var run = function(in_file, out_file) { 
+
+	var file = fs.readFileSync(in_file, {encoding: 'utf8'});
+	file = file.split('\n');
+	var split_strings = [];
+
+	/* line 1 is number of test cases */
+	var N = parseInt(file[0]);
+	file = file.splice(1, file.length);
+
+	/* each three lines after, C I P are a set */
+	var sets = [];
+	for(var i = 0; i < file.length; i++) {
+		var line = file[i].split(' ');
+		sets.push({C: parseFloat(line[0]), F: parseFloat(line[1]), X: parseFloat(line[2])});
+	}
+	
+	for(var i = 0; i < N; i++) {
+		var set = sets[i];
+		console.log(set);
+		split_strings.push("Case #" + (i+1) + ": " + cookie(set));	
+	}
+	
+	fs.writeFileSync(out_file, split_strings.join('\n'), 'utf8');
+
+}
+
+
+run('./B-mini-practice.in', './B-mini-practice.out');
+run('./B-small-practice.in', './B-small-practice.out');
+run('./B-large-practice.in', './B-large-practice.out');
